9049ae4f7ef5f7cfc1c4cb848cb671f0b1d5b964
Nickolas Burr <nickolasburr@gmail.com>
2018-07-17 14:44:55 -0400

    Add error handler goto, adjust range alloc

--- src/main.c
+++ src/main.c
@@ -14,9 +14,11 @@
 int main (int argc, char **argv) {
 	int index, zindex;
 	int count, start, end, opt_value, long_opt_index;
-	char *line, *range, *token;
 	size_t len;
 	ssize_t read;
+	char *line = NULL,
+	     *range = NULL,
+	     *token = NULL;
 	FILE *stream = NULL;
 
 	long_opt_index = 0;
@@ -58,7 +60,7 @@ int main (int argc, char **argv) {
 	for (index = 1; index < argc; index += 1) {
 		switch (index) {
 			case 1:
-				range = ALLOC(sizeof(argv[index]) + NULL_BYTE);
+				range = ALLOC(sizeof(char) * (length(argv[index]) + NULL_BYTE));
 				copy(range, argv[index]);
 
 				zindex = 0;
@@ -68,7 +70,7 @@ int main (int argc, char **argv) {
 						fprintf(stderr, "%s: '%s' is not a valid range.\n\n", PROGNAME, argv[index]);
 						usage();
 
-						exit(EXIT_FAILURE);
+						goto on_error;
 					}
 
 					switch (zindex) {
@@ -92,7 +94,7 @@ int main (int argc, char **argv) {
 				if (!is_file(argv[index])) {
 					fprintf(stderr, "%s: '%s' is not a valid filename.\n\n", PROGNAME, argv[index]);
 
-					exit(EXIT_FAILURE);
+					goto on_error;
 				}
 
 				stream = fopen(argv[index], "r");
@@ -121,4 +123,9 @@ int main (int argc, char **argv) {
 	}
 
 	return EXIT_SUCCESS;
+
+on_error:
+	FREE(range);
+
+	return EXIT_FAILURE;
 }
